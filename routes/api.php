6b53c6307eec83bf05126ce98782b581a8d45495
Yu-hanCheng <sarahcheng1231@gmail.com>
2019-11-05 18:54:44 +0800

    api basic

--- routes/api.php
+++ routes/api.php
@@ -12,17 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('register', 'UserController@create');
+Route::post('register', 'UserController@store');
 
 
-Route::group(['middleware' => ['auth.merchant']], function(){
+Route::group(['middleware' => ['auth.user:merchant']], function(){
     Route::get('merchant/logout', 'UserController@logout');
     Route::post('merchant/login', 'UserController@login');
-    Route::resource('merchant/shop', 'ShopController')->only(['index', 'store', 'show']);
+    Route::resource('merchant/shop', 'ShopController')->only(['index', 'store', 'show','destroy']);
     Route::resource('merchant/shopmagic', 'ShopMagicController')->only(['index', 'store', 'show']);
     Route::resource('merchant/magic', 'MagicController')->only(['index', 'store', 'show']);
 });
-Route::group(['middleware' => ['auth.user']], function(){
+Route::group(['middleware' => ['auth.user:user']], function(){
     Route::get('logout', 'UserController@logout');
     Route::post('login', 'UserController@login');
     Route::resource('shop', 'ShopController')->only(['index', 'show']);
