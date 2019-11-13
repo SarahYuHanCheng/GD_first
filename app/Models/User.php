f61f47e5b7fbccd4f1cb40a30a96fd4548540e9f
Yu-hanCheng <sarahcheng1231@gmail.com>
2019-11-13 17:08:57 +0800

    User store validate, balance

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'password','role','remember_token'
+        'name', 'password','role','remember_token','balance'
     ];
 
     /**
