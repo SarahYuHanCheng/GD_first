bffc0f65094f118623760d617f16590b8053cccf
Yu-hanCheng <sarahcheng1231@gmail.com>
2019-11-13 17:28:03 +0800

    AuthToken validate

--- app/Http/Middleware/AuthTokenSarah.php
+++ app/Http/Middleware/AuthTokenSarah.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 use App\Providers\TokenServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Validator;
 class AuthTokenSarah
 {
     
@@ -23,14 +23,25 @@ class AuthTokenSarah
     {
         $out = new \Symfony\Component\Console\Output\ConsoleOutput();
         $out->writeln("* in auth handle:* ");
-        try {
-            $credentials = $request->only('remember_token','password');
-            if (Auth::attempt($credentials,true)) {
-                $out->writeln("* auth ok:* ");
-                $request->merge(['user' => Auth::user()]);
-            }else {
-                $out->writeln("* no remember:* ");
-                try {
+
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:15',
+            'password' => 'required|max:15',
+        ]);
+        
+        if ($validator->fails()) {
+            $out->writeln('failed: '.$validator->messages());
+            return response()->json(['result'=>$validator->messages()],406);
+        }else {
+                
+            try {
+                $credentials = $request->only('remember_token','password');
+                if (Auth::attempt($credentials,true)) {
+                    $out->writeln("* auth ok:* ");
+                    $request->merge(['user' => Auth::user()]);
+                }else {
+                    $out->writeln("* no remember:* ");
                     $credentials = $request->only('name', 'password');
                     if (Auth::attempt($credentials,true)) {
                         // Authentication passed...
@@ -41,23 +52,20 @@ class AuthTokenSarah
                         $request->merge(['user' => Auth::user()]);
                         
                     }else {
-                        return response()->json(['result'=>'please login first.'],401);
+                        return response()->json(['result'=>'name or password error.'],401);
                     }
-                } catch (\Throwable $th) {
-                    $out->writeln("* attempt error:* ".$th);
-                    return response()->json(['result'=>'wrong name or password.'],401);
                 }
-            }
-            if($role ==  'merchant'){
-                if( Auth::user()->role != '1'){
-                    return response()->json(['result'=>'Can\'t access the url.'],403);
+                if($role ==  'merchant'){
+                    if( Auth::user()->role != '1'){
+                        return response()->json(['result'=>'Can\'t access the url.'],403);
+                    }
                 }
-            }
+                
+                return $next($request);
+            } catch (\Throwable $th) {
+                $out->writeln("* first error:* ".$th);
             
-            return $next($request);
-        } catch (\Throwable $th) {
-            $out->writeln("* first error:* ".$th);
-        
+            }
         }
 
         
